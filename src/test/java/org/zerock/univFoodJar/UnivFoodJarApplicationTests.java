bb81a4ecd616d8d92ef437a6deb2995ba86ed1fd
Kimhyunjin4455 <dp4455@naver.com>
2024-06-07 15:08:23 +0900

    first commit

---
+++ src/test/java/org/zerock/univFoodJar/UnivFoodJarApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.univFoodJar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnivFoodJarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
